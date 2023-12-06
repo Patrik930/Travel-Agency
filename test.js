7e0005f316abf2ca4ff1ae5672f4e15c2e3c613a
RPatrik <focimc02@gmail.com>
2023-12-06 08:51:00 +0100

    Text + index updated

--- test.js
+++ test.js
@@ -11,6 +11,14 @@ const showTestsResults=(assert,result)=>{
         olDOM.innerHTML+=`<li>${assert} ❌</li>`
 }
 
-let assert = "h5 (5-ös szintű címsor) létrehozva";
-let h5 = document.querySelector("h5");
-console.log(showTestsResults(assert,h5))
+let assert = "h1 (1-es szintű címsor) létrehozva";
+let h1 = document.querySelector("h1");
+console.log(showTestsResults(assert,h1))
+
+assert = "1-es szintű címsor tartalma a mintának megfelelő";
+showTestsResults(assert,h1?.textContent == "Arany Napfény Utazási Iroda")
+
+assert = "Az 1-es címsor betűszíne arany."
+let h1Style = window.getComputedStyle(h1)
+console.log(h1Style.color)
+showTestsResults(assert, h1Style?.color == "rgb(255, 215, 0)")
